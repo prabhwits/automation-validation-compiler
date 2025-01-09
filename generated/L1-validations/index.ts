9b38a8ed4e0630bde2a454b32cbd9288570348f9
extedcouD <extedcoud@gmail.com>
2025-01-09 12:42:51 +0530

    transfer

--- generated/L1-validations/index.ts
+++ generated/L1-validations/index.ts
@@ -1,16 +1,10 @@
 import search from "./api-tests/search";
-import select from "./api-tests/select";
-import init from "./api-tests/init";
-import confirm from "./api-tests/confirm";
-import status from "./api-tests/status";
-import cancel from "./api-tests/cancel";
 import on_search from "./api-tests/on_search";
+import select from "./api-tests/select";
 import on_select from "./api-tests/on_select";
+import init from "./api-tests/init";
 import on_init from "./api-tests/on_init";
-import on_confirm from "./api-tests/on_confirm";
-import on_cancel from "./api-tests/on_cancel";
-import on_update from "./api-tests/on_update";
-import on_status from "./api-tests/on_status";
+import confirm from "./api-tests/confirm";
 
 export function performL1Validations(
     action: string,
@@ -27,48 +21,16 @@ export function performL1Validations(
                     runAllValidations: allErrors,
                 },
             });
-        case "select":
-            return select({
-                payload: payload,
-                externalData: externalData,
-                config: {
-                    runAllValidations: allErrors,
-                },
-            });
-        case "init":
-            return init({
-                payload: payload,
-                externalData: externalData,
-                config: {
-                    runAllValidations: allErrors,
-                },
-            });
-        case "confirm":
-            return confirm({
-                payload: payload,
-                externalData: externalData,
-                config: {
-                    runAllValidations: allErrors,
-                },
-            });
-        case "status":
-            return status({
-                payload: payload,
-                externalData: externalData,
-                config: {
-                    runAllValidations: allErrors,
-                },
-            });
-        case "cancel":
-            return cancel({
-                payload: payload,
-                externalData: externalData,
-                config: {
-                    runAllValidations: allErrors,
-                },
-            });
         case "on_search":
             return on_search({
+                payload: payload,
+                externalData: externalData,
+                config: {
+                    runAllValidations: allErrors,
+                },
+            });
+        case "select":
+            return select({
                 payload: payload,
                 externalData: externalData,
                 config: {
@@ -83,6 +45,14 @@ export function performL1Validations(
                     runAllValidations: allErrors,
                 },
             });
+        case "init":
+            return init({
+                payload: payload,
+                externalData: externalData,
+                config: {
+                    runAllValidations: allErrors,
+                },
+            });
         case "on_init":
             return on_init({
                 payload: payload,
@@ -91,32 +61,8 @@ export function performL1Validations(
                     runAllValidations: allErrors,
                 },
             });
-        case "on_confirm":
-            return on_confirm({
-                payload: payload,
-                externalData: externalData,
-                config: {
-                    runAllValidations: allErrors,
-                },
-            });
-        case "on_cancel":
-            return on_cancel({
-                payload: payload,
-                externalData: externalData,
-                config: {
-                    runAllValidations: allErrors,
-                },
-            });
-        case "on_update":
-            return on_update({
-                payload: payload,
-                externalData: externalData,
-                config: {
-                    runAllValidations: allErrors,
-                },
-            });
-        case "on_status":
-            return on_status({
+        case "confirm":
+            return confirm({
                 payload: payload,
                 externalData: externalData,
                 config: {
